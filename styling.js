65ec066317f367c4f12d6ab7a1f9e99cc4fdaac1
adtoria <adityyyyadav@gmail.com>
2023-10-18 15:35:35 +0530

    styling

---
+++ styling.js
@@ -0,0 +1,12 @@
+const title = document.querySelector('h1');
+
+// title.setAttribute('style', 'margin: 50px;');
+// overwrites what was previously there
+
+console.log(title.style);
+console.log(title.style.color);
+
+title.style.margin = '50px';
+title.style.color = 'crimson';
+title.style.fontSize = '60px';
+title.style.margin = ''; // to remove a property
